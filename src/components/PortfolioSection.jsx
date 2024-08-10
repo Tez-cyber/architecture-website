6290860a746c11631f502aa6b8f0ea712d52e957
Salami Azeez <salamitopeazeez@gmail.com>
2024-08-10 16:18:11 +0100

    Adjusted PortfolioSection.jsx head

--- src/components/PortfolioSection.jsx
+++ src/components/PortfolioSection.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import Img1 from "../assets/port-1.jpg"
 import Img2 from "../assets/port-2.jpg"
-import Img3 from "../assets/port-4.jpg"
+import Img3 from "../assets/port-new.jpg"
 
 const PortfolioSection = () => {
     const images = [
@@ -33,9 +33,9 @@ const PortfolioSection = () => {
     return (
         <div className="">
             {/* ======== Heading ========= */}
-            <div className="py-20 flex items-center justify-center gap-2 text-3xl uppercase tracking-widest">
-                <h2>Hold up. Check out our amazing</h2>
-                <h2 className='text-darkCream'>projects.</h2>
+            <div className="py-20 text-center items-center justify-center gap-2 text-3xl uppercase tracking-widest md:flex">
+                <h2>Hold up. Check out our amazing <span className='text-darkCream md:hidden'>projects.</span></h2>
+                <h2 className='text-darkCream hidden md:block'>projects.</h2>
             </div>
             {/* ========= Content ========= */}
             <div className="parent w-3/4 mx-auto">
@@ -73,30 +73,30 @@ const PortfolioSection = () => {
                     </div> */}
                     <div className="grid grid-cols-3 gap-2">
                         <div className="">
-                            <img src={Img1} className='rounded-xl' alt="" />
+                            <img src={Img1} className='port-img' alt="" />
                         </div>
                         <div className="">
-                            <img src={Img1} className='rounded-xl' alt="" />
+                            <img src={Img3} className='port-img' alt="" />
                         </div>
                         {/* =========== */}
                         <div className="flex flex-col gap-2">
                             <div className="">
-                                <img src={Img2} className='rounded-xl ' alt="" />
+                                <img src={Img3} className='port-img ' alt="" />
                             </div>
                             <div className="">
-                                <img src={Img2} className='rounded-xl ' alt="" />
+                                <img src={Img3} className='port-img ' alt="" />
                             </div>
                         </div>
                     </div>
                     <div className="grid grid-cols-3 gap-2">
                         <div className="">
-                            <img src={Img1} className='rounded-xl' alt="" />
+                            <img src={Img1} className='port-img' alt="" />
                         </div>
                         <div className="">
-                            <img src={Img1} className='rounded-xl' alt="" />
+                            <img src={Img1} className='port-img' alt="" />
                         </div>
                         <div className="">
-                            <img src={Img1} className='rounded-xl' alt="" />
+                            <img src={Img1} className='port-img' alt="" />
                         </div>
                     </div>
                 </div>
