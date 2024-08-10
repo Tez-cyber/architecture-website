1116209bad4db27e0bde19e7d133bc5caca195d7
Salami Azeez <salamitopeazeez@gmail.com>
2024-08-10 15:41:24 +0100

    Added image for portfolio

--- src/components/PortfolioSection.jsx
+++ src/components/PortfolioSection.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
-import Img1 from "../assets/img-4.jpg"
-// import Img2 from "../assets/img-1.jpg"
+import Img1 from "../assets/port-1.jpg"
+import Img2 from "../assets/port-2.jpg"
+import Img3 from "../assets/port-4.jpg"
 
 const PortfolioSection = () => {
     const images = [
@@ -37,13 +38,67 @@ const PortfolioSection = () => {
                 <h2 className='text-darkCream'>projects.</h2>
             </div>
             {/* ========= Content ========= */}
-            <div className="parent flex justify-center items-center">
-                <div className="container relative grid">
-                    <div className="card">
-                        <img src={Img1} alt="" />
+            <div className="parent w-3/4 mx-auto">
+                <div className=" flex flex-col gap-2">
+                    {/* <div className="">
+                        <div className="">
+                            <img src={Img1} className='w-1/2' alt="" />
+                        </div>
+                        <div className="">
+                            <img src={Img1} className='w-1/2' alt="" />
+                        </div>
                     </div>
-                    <div className="card"></div>
-                    <div className="card"></div>
+                    <div className="">
+                        <div className="grid grid-cols-2">
+                            <div className="">
+                                <img src={Img1} className='w-full' alt="" />
+                            </div>
+                            <div className="">
+                                <div className="">
+                                    <img src={Img1} className='' alt="" />
+                                </div>
+                                <div className="">
+                                    <img src={Img1} className='' alt="" />
+                                </div>
+                            </div>
+                        </div>
+                        <div className="grid grid-cols-2">
+                            <div className="">
+                                <img src={Img1} className='' alt="" />
+                            </div>
+                            <div className="">
+                                <img src={Img1} className='' alt="" />
+                            </div>
+                        </div>
+                    </div> */}
+                    <div className="grid grid-cols-3 gap-2">
+                        <div className="">
+                            <img src={Img1} className='rounded-xl' alt="" />
+                        </div>
+                        <div className="">
+                            <img src={Img1} className='rounded-xl' alt="" />
+                        </div>
+                        {/* =========== */}
+                        <div className="flex flex-col gap-2">
+                            <div className="">
+                                <img src={Img2} className='rounded-xl ' alt="" />
+                            </div>
+                            <div className="">
+                                <img src={Img2} className='rounded-xl ' alt="" />
+                            </div>
+                        </div>
+                    </div>
+                    <div className="grid grid-cols-3 gap-2">
+                        <div className="">
+                            <img src={Img1} className='rounded-xl' alt="" />
+                        </div>
+                        <div className="">
+                            <img src={Img1} className='rounded-xl' alt="" />
+                        </div>
+                        <div className="">
+                            <img src={Img1} className='rounded-xl' alt="" />
+                        </div>
+                    </div>
                 </div>
             </div>
         </div>
