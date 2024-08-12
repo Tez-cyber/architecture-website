461e62ff697a80690cfb895e51f203359cb7b19f
Salami Azeez <salamitopeazeez@gmail.com>
2024-08-12 08:40:42 +0100

    Adjusted image size and changed vision text content to white

--- src/components/Vision.jsx
+++ src/components/Vision.jsx
@@ -2,21 +2,23 @@ import React from 'react'
 import visImg from '../assets/port-new.jpg'
 
 const Vision = () => {
-  return (
-    <div className=''>
-        {/* ==== Image section ==== */}
-        <div className="">
-            <img src={visImg} alt="" />
+    return (
+        <div className='bg-darkBrown'>
+            <div className="flex gap-10 lg:py-10 lg:w-[1024px] lg:mx-auto">
+                {/* ==== Image section ==== */}
+                <div className="">
+                    <img src={visImg} className='' alt="" />
+                </div>
+                {/* ==== Content section ==== */}
+                <div className="text-white">
+                    <h2>Let us turn your vision into reality</h2>
+                    <p className="">
+                        <span className=""></span> delivers modular building solutions that blend efficiency with premium aesthetics, ensuring each project is seamless and impactful. Our approach allows you to meet tight timelines and high expectations with ease, while turning your creative vision into reality.
+                    </p>
+                </div>
+            </div>
         </div>
-        {/* ==== Content section ==== */}
-        <div className="">
-            <h2>Let us turn your vision into reality</h2>
-            <p className="">
-                <span className=""></span> delivers modular building solutions that blend efficiency with premium aesthetics, ensuring each project is seamless and impactful. Our approach allows you to meet tight timelines and high expectations with ease, while turning your creative vision into reality.
-            </p>
-        </div>
-    </div>
-  )
+    )
 }
 
 export default Vision
