d3d5fb5e83bea4239ecf15ded6dfe1c05bac93e5
Salami Azeez <salamitopeazeez@gmail.com>
2024-08-13 10:32:52 +0100

    Added navlinks in index.js

---
+++ src/constants/index.js
@@ -0,0 +1,7 @@
+
+export const navlinks = [
+    { url: "#", title: "Home" },
+    { url: "#about", title: "About" },
+    { url: "#portfolio", title: "Portfolio" },
+    { url: "#contact", title: "Contact" }
+]
