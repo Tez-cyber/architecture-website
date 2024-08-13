d5d7256f3714d203b6c59b141ac585aab3ba93b6
Salami Azeez <salamitopeazeez@gmail.com>
2024-08-13 10:31:24 +0100

    Added new folder with index.js

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,12 +1,32 @@
 import React from 'react'
 
 const Footer = () => {
-  return (
-    <div className="">
-        <div className=""></div>
-        <div className=""></div>
-    </div>
-  )
+    const links = [
+        { url: "#", title: "Home" },
+        { url: "#about", title: "About" },
+        { url: "#portfolio", title: "Portfolio" },
+        { url: "#contact", title: "Contact" }
+    ]
+
+    return (
+        <div className="bg-darkBrown">
+            <div className="">
+                <p className="">
+                    Empowering premium brands through modular spaces
+                </p>
+            </div>
+            <div className="">
+                <div className="">
+                    <div className="">
+
+                    </div>
+                    <div className=""></div>
+                    <div className=""></div>
+                </div>
+                <div className=""></div>
+            </div>
+        </div>
+    )
 }
 
 export default Footer
