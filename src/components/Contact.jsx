a89bc724b7569eb85b8778c37f67421556616b75
Salami Azeez <salamitopeazeez@gmail.com>
2024-08-14 14:41:09 +0100

    Completed first section for contact section

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,9 +1,27 @@
 import React from 'react'
 
 const Contact = () => {
-  return (
-    <div>Contact</div>
-  )
+    return (
+        <div className="">
+            <div className="py-10 lg:w-[1156px] lg:mx-auto lg:flex-row    ">
+                {/* ==== First Section ==== */}
+                <div className="flex flex-col gap-2 lg:w-[30%]">
+                    <p className='text-darkCream text-[15px]'>Contact</p>
+                    <p className="text-2xl lg:text-6xl">
+                        Challenge us with
+                        your project
+                    </p>
+                    <p className="pt-6 text-xl">
+                        See how leading organizations have trusted us
+                        to meet their challenging designs and logistical
+                        requirements with precision.
+                    </p>
+                </div>
+                {/* ==== Second Section ==== */}
+                <div className=""></div>
+            </div>
+        </div>
+    )
 }
 
 export default Contact
