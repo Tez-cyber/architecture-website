70b8656f27f60ad10d7fe4e6f833145183933290
Salami Azeez <salamitopeazeez@gmail.com>
2024-08-15 23:07:14 +0100

    Added first element for values section

--- src/components/Values.jsx
+++ src/components/Values.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
-
+import Img1 from '../assets/img-2.jpg'
 const Values = () => {
   return (
-    <div>Values</div>
+    <main className='app'>
+        <div className="section_container">
+            {/* ===== First Element */}
+            <div className="section_col">
+                <div className="section_col_media">
+                    <img src={Img1} alt="" />
+                </div>
+                <div className="section_col_caption">
+                    <span>Push the boundaries of traditional design</span>
+                </div>
+                <div className="section_col_title">
+                    <h2 className="">Elevate</h2>
+                    <p className="">Discover More</p>
+                </div>
+                <div className="section_col_number">
+                    <h2 className="">1</h2>
+                </div>
+            </div>
+        </div>
+    </main>
   )
 }
 
