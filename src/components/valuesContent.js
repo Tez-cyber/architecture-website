3eee4b8b717ff8ffb9162da702417db844fac4cc
Salami Azeez <salamitopeazeez@gmail.com>
2024-09-20 15:38:31 +0100

    Created valueContent with data for value second section

---
+++ src/components/valuesContent.js
@@ -0,0 +1,27 @@
+
+export const valueContent = [
+    {
+        img: "../assets/port-1.jpg",
+        title: "Push the boundaries of traditional design",
+        name: "Elevate",
+        id: "1"
+    },
+    {
+        img: "../assets/port-1.jpg",
+        title: "Trascending the ordinary and introducing innovation",
+        name: "Innovate",
+        id: "2"
+    },
+    {
+        img: "../assets/port-1.jpg",
+        title: "Challenge conventions and redefine the boundaries",
+        name: "Reimagine",
+        id: "3"
+    },
+    {
+        img: "../assets/port-1.jpg",
+        title: "Engage with spaces designed to fully captivate and envelope",
+        name: "Immerse",
+        id: "4"
+    },
+]
