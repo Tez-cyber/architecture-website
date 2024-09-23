eb8c2e87e4331901fee5a0f4d4f9908b7b00c648
Salami Azeez <salamitopeazeez@gmail.com>
2024-09-23 13:24:29 +0100

    Added images folder in public and import in valuesContent

--- src/components/valuesContent.js
+++ src/components/valuesContent.js
@@ -1,39 +1,39 @@
 
 export const valueContents = [
     {
-        img: "./src/assets/port-1.jpg",
+        img: "/public/images/val-1.jpg",
         title: "",
         name: "",
         id: "0"
     },
     {
-        img: "./src/assets/port-1.jpg",
+        img: "/public/images/val-2.jpg",
         title: "Push the boundaries of traditional design",
         name: "Elevate",
         id: "1"
     },
     {
-        img: "./src/assets/port-1.jpg",
+        img: "/public/images/val-1.jpg",
         title: "Trascending the ordinary and introducing innovation",
         name: "Innovate",
         id: "2"
     },
     {
-        img: "./src/assets/port-1.jpg",
+        img: "/public/images/val-2.jpg",
         title: "Challenge conventions and redefine the boundaries",
         name: "Reimagine",
         id: "3"
     },
     {
-        img: "./src/assets/port-1.jpg",
+        img: "/public/images/val-1.jpg",
         title: "Engage with spaces designed to fully captivate and envelope",
         name: "Immerse",
         id: "4"
     },
     {
-        img: "./src/assets/port-1.jpg",
+        img: "/public/images/val-1.jpg",
         title: "",
         name: "",
-        id: "0"
+        id: "00"
     }
 ]
