35a29684d72091bbc98eb9ef7b28fe199b7d149f
Salami Azeez <salamitopeazeez@gmail.com>
2024-09-23 14:11:02 +0100

    Changed img url

--- src/components/valuesContent.js
+++ src/components/valuesContent.js
@@ -1,37 +1,37 @@
 
 export const valueContents = [
     {
-        img: "/val-1.jpg",
+        img: "/src/assets/port-1.jpg",
         title: "",
         name: "",
         id: "0"
     },
     {
-        img: "/port-1.jpg",
+        img: "/src/assets/port-1.jpg",
         title: "Push the boundaries of traditional design",
         name: "Elevate",
         id: "1"
     },
     {
-        img: "/port-1.jpg",
+        img: "/src/assets/port-1.jpg",
         title: "Trascending the ordinary and introducing innovation",
         name: "Innovate",
         id: "2"
     },
     {
-        img: "/port-1.jpg",
+        img: "/src/assets/port-1.jpg",
         title: "Challenge conventions and redefine the boundaries",
         name: "Reimagine",
         id: "3"
     },
     {
-        img: "/port-1.jpg",
+        img: "/src/assets/port-1.jpg",
         title: "Engage with spaces designed to fully captivate and envelope",
         name: "Immerse",
         id: "4"
     },
     {
-        img: "/port-1.jpg",
+        img: "/src/assets/port-1.jpg",
         title: "",
         name: "",
         id: "00"
